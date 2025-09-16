062f799ca0ef58dea2669c6664282fe7a58662ac
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-09-17 00:20:51 +0900

    feat : 클릭시 텍스트 색상변경

--- 심씨-심규석/Week_1/Base/app/src/main/java/com/umc/myapplication/MainActivity.kt
+++ 심씨-심규석/Week_1/Base/app/src/main/java/com/umc/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.umc.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,20 +13,28 @@ import com.umc.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageViews : List<ImageView>
+    private lateinit var textViews : List<TextView>
+    private val colors = listOf(Color.YELLOW, Color.CYAN, Color.BLUE, Color.GREEN, Color.RED)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        imageViews = listOf(binding.imageView1, binding.imageView2, binding.imageView3, binding.imageView4, binding.imageView5)
+        textViews = listOf(binding.textView1, binding.textView2, binding.textView3, binding.textView4, binding.textView5)
 
+        imageViews.mapIndexed { imageIndex, imageView ->
+            imageView.setOnClickListener {
+                textViews.mapIndexed { textIndex, textView ->
+                    if(imageIndex == textIndex) textView.setTextColor(colors.get(imageIndex))
+                    else textView.setTextColor(Color.BLACK)
+                }
 
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            }
+
         }
     }
 }
