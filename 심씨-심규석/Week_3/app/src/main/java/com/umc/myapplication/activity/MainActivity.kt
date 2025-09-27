0650e9336f27730556c124f75dab21d135ff871b
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-09-28 02:22:35 +0900

    feat : bottom nav에 필수요소 생성

--- 심씨-심규석/Week_3/app/src/main/java/com/umc/myapplication/activity/MainActivity.kt
+++ 심씨-심규석/Week_3/app/src/main/java/com/umc/myapplication/activity/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.umc.myapplication.R
 import com.umc.myapplication.databinding.ActivityMainBinding
 
@@ -15,15 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+
+        //FragmentContainerView에 연결된 NavController
+        //bing.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // BottomNavigationView를 NavController와 연결
+        binding.bottomNavigationView.setupWithNavController(navController)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
