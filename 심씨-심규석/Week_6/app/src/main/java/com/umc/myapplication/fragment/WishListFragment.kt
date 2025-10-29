65cf52bb59c7dbe3801686441f0b55cdf110ffeb
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-10-29 10:37:46 +0900

    feat : 6주차 init

---
+++ 심씨-심규석/Week_6/app/src/main/java/com/umc/myapplication/fragment/WishListFragment.kt
@@ -0,0 +1,46 @@
+package com.umc.myapplication.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.umc.myapplication.R
+import com.umc.myapplication.adapter.SearchProductAdapter
+import com.umc.myapplication.adapter.WishListProductAdapter
+import com.umc.myapplication.databinding.FragmentHomeBinding
+import com.umc.myapplication.databinding.FragmentWishListBinding
+import com.umc.myapplication.testData.testProductRepository
+
+class WishListFragment : Fragment() {
+    private var _binding: FragmentWishListBinding? = null
+    private val binding get() = _binding!!
+
+    val list = testProductRepository.products.filter { it.isWishList }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWishListBinding.inflate(inflater, container, false)
+
+        binding.recycler.adapter = WishListProductAdapter(list) {
+            val action = WishListFragmentDirections
+                .actionWishListFragmentToProductDetailFragment(
+                    productId = it.productId
+                )
+            findNavController().navigate(action)
+        }
+        binding.recycler.layoutManager = GridLayoutManager(context, 2)
+        
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
