6fff9a234605565257837b4da0d0837faf9e3036
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-11-02 02:45:36 +0900

    feat : 로그인 로직 완성

--- 심씨-심규석/Week_6/app/src/main/java/com/umc/myapplication/activity/MainActivity.kt
+++ 심씨-심규석/Week_6/app/src/main/java/com/umc/myapplication/activity/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.umc.myapplication.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.umc.myapplication.R
 import com.umc.myapplication.databinding.ActivityMainBinding
+import com.umc.myapplication.viewmodel.AuthViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<AuthViewModel>()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +27,22 @@ class MainActivity : AppCompatActivity() {
 
 
         //FragmentContainerView에 연결된 NavController
-        //bing.
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // userId 변화에 따라 그래프/네비게이션 전환
+        viewModel.userId.observe(this) { id ->
+            if (id.isNullOrEmpty()) {
+                // 비로그인: 웰컴 그래프 로드, BottomNav 숨김
+                navController.setGraph(R.navigation.nav_graph_welcome)
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                // 로그인: 메인 그래프 로드, BottomNav 표시
+                navController.setGraph(R.navigation.nav_graph_main)
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
 
         // BottomNavigationView를 NavController와 연결
         binding.bottomNavigationView.setupWithNavController(navController)
