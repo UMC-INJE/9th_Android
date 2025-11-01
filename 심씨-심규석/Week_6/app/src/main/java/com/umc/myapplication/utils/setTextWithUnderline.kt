2b93691604aecd168f36c88e0d136434d0ec63c4
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-11-02 02:43:40 +0900

    feat : 회원가입, 로그인 viewmodel 생성

--- 심씨-심규석/Week_6/app/src/main/java/com/umc/myapplication/utils/setTextWithUnderline.kt
+++ 심씨-심규석/Week_6/app/src/main/java/com/umc/myapplication/utils/setTextWithUnderline.kt
@@ -1,26 +1,52 @@
 package com.umc.myapplication.utils
 
+import android.annotation.SuppressLint
 import android.text.SpannableString
 import android.text.Spanned
-import android.text.style.ForegroundColorSpan
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.text.style.UnderlineSpan
+import android.view.View
 import android.widget.TextView
+import com.umc.myapplication.R
 
-fun TextView.setTextWithUnderline(
-    base: String,
-    underlined: String,
-    color: Int? = null
-): SpannableString {
-    // base 바로 뒤에 underlined를 붙인다
-    val full = base + underlined
-    val start = full.length - underlined.length
-    val end = full.length
+@SuppressLint("ResourceAsColor")
+fun setUnderlinedSpannable(
+    textView: TextView,
+    fullText: String,
+    underlineTargets: List<String>,
+    textColor: Int = R.color.black,
+    textSizeSp: Float = 14f,
+    clickActions: Map<String, (() -> Unit)> = emptyMap()
+) {
+    val spannable = SpannableString(fullText)
+    underlineTargets.forEach { target ->
+        val start = fullText.indexOf(target)
+        if (start >= 0) {
+            val end = start + target.length
+            val underlineSpan = UnderlineSpan()
+            spannable.setSpan(underlineSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-    val text = SpannableString(full).apply {
-        setSpan(UnderlineSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        if (color != null) {
-            setSpan(ForegroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            // 클릭 동작 추가
+            if (clickActions.containsKey(target)) {
+                val clickableSpan = object : ClickableSpan() {
+                    override fun onClick(widget: View) {
+                        clickActions[target]?.invoke()
+                    }
+                    @SuppressLint("ResourceAsColor")
+                    override fun updateDrawState(ds: TextPaint) {
+                        super.updateDrawState(ds)
+                        ds.color = textColor // 밑줄 링크도 동일 색상
+                        ds.isUnderlineText = true
+                    }
+                }
+                spannable.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
         }
     }
-    return text
+    textView.text = spannable
+    textView.setTextColor(textColor)
+    textView.textSize = textSizeSp
+    textView.movementMethod = LinkMovementMethod.getInstance()
 }
