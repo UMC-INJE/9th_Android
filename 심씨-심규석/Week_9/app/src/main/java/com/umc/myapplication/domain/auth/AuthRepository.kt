d47e681651b0d4a4ae5f16e24023e6dab09e0b8a
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-11-30 13:07:42 +0900

    refactor : 안올라간것 한번 더 올림

--- 심씨-심규석/Week_9/app/src/main/java/com/umc/myapplication/domain/auth/AuthRepository.kt
+++ 심씨-심규석/Week_9/app/src/main/java/com/umc/myapplication/domain/auth/AuthRepository.kt
@@ -1,10 +1,14 @@
 package com.umc.myapplication.domain.auth
 
+import com.umc.myapplication.data.models.request.SignInRequest
+import com.umc.myapplication.data.models.request.SignUpRequest
 import com.umc.myapplication.domain.model.User
 
 interface AuthRepository {
 
-    suspend fun signUpWithEmail(email: String, password: String): Result<User>
-    suspend fun signInWithEmail(email: String, password: String): Result<User>
+    suspend fun signUpWithEmail(form : SignUpRequest): Result<User>
+    suspend fun signInWithEmail(form : SignInRequest): Result<User>
     suspend fun signOut(): Result<Unit>
+
+    fun getCurrentUser(): User?
 }
