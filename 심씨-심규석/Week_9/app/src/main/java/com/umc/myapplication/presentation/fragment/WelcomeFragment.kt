0781bb409e02bbed43378fc9028cf7cd8d700e75
sfpahs <119095356+sfpahs@users.noreply.github.com>
2025-12-21 21:52:13 +0900

    feat : 카톡로그인 로직생성
    메인화면 카톡로그인 버튼,
    로그인 로직 만듬

--- 심씨-심규석/Week_9/app/src/main/java/com/umc/myapplication/presentation/fragment/WelcomeFragment.kt
+++ 심씨-심규석/Week_9/app/src/main/java/com/umc/myapplication/presentation/fragment/WelcomeFragment.kt
@@ -1,18 +1,23 @@
 package com.umc.myapplication.presentation.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.umc.myapplication.databinding.FragmentWelcomeBinding
+import com.umc.myapplication.presentation.feature.AuthViewModel
+import com.umc.myapplication.util.kakaoLogin
 
 class WelcomeFragment : Fragment() {
 
     private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
-
+    private val viewModel : AuthViewModel by activityViewModels()
+    private val TAG = "WelcomeFragment"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +31,15 @@ class WelcomeFragment : Fragment() {
         binding.signUpButton.setOnClickListener {
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToSignUpFragment()
             findNavController().navigate(action)
+        }
+        binding.kakaoLogin.setOnClickListener {
+            kakaoLogin(requireContext(),
+                onSuccess = {
+                    viewModel.signInWithKakao(it)
+                }
+            , onError = {
+                    Log.e(TAG, "onCreateView: ${it?.message}", )
+                })
         }
         return binding.root
     }
